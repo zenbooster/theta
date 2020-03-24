e3f5ae6b2e1c790a2a50b5700ab19f89f260e9d5
Ilya Goncharov <Igoncharov@aladdin.ru>
2020-03-24 11:44:18 +0300

    заменил уголки у шелмаунтов

--- euc.py
+++ euc.py
@@ -13,6 +13,7 @@ con2020d = from_brep('.\\brep\\con2020d.brep')
 con2020 = from_brep('.\\brep\\con2020.brep').left(10).down(6.25).rotateY(deg(-90)).rotateX(deg(180))
 #con2040 = from_brep('.\\brep\\con2040d.brep').right(38.1/2).back(38.1/2).down(17.4/2).rotateX(deg(90)).rotateZ(deg(-90)).rotateX(deg(-90))
 #con2040 = from_brep('.\\brep\\con2040d.brep').right(38.1/2).back(38.1/2).down(17.4/2)#.rotateZ(deg(-90))
+con2040s = from_brep('.\\brep\\con2040s.brep').down(18/2 - 2).left(38.5/2).back(38.5/2).rotateX(deg(90)).rotateY(deg(180))
 con4040s = from_brep('.\\brep\\con4040s.brep').down((38.5-3.7)/2).rotateX(deg(90)).left(38.5/2).down(38.5/2)
 
 ddt = dropout_depth - 20
@@ -22,37 +23,6 @@ gap_dropout_width = gap(dropout_width, 2)
 gap_dropout_height = gap(dropout_height)
 gap_dropout_depth = gap(dropout_depth)
 gap_dropout_sole_pos = gap(dropout_sole_pos)
-
-spit_dt = 8.9
-
-'''
-def get_side_compartment(is_right):
-    res = side_compartment_base
-    cut = sm.get_shell_mount_holes().down(side_compartment_height/2).forw(side_compartment_depth/2-sm.top_mount_depth/2)
-    cut += get_top_spacer_holls().up(side_compartment_height/2).back(top_height/2-side_compartment_depth/2)
-
-    if is_right:
-        cut = cut.mirrorYZ()
-
-    res -= cut
-    
-    if is_right:
-        res = res.rotateZ(deg(180))
-
-    return res;
-    
-def get_upper_compartment():
-    res = j1550
-    tsh = get_top_spacer_holls().down(side_compartment_depth/2)
-    res -= tsh.back((wheel_arch_width+top_height)/2)
-    res -= tsh.forw((wheel_arch_width+top_height)/2)
-
-    #d = hole_d[10] 
-    #nut = ngon((16 * 2 / sqrt(3))/2, 6, False).extrude(10).down(side_compartment_inner_depth/2)
-    #res += nut.back((wheel_arch_width+top_height)/2).left(side_compartment_inner_width/2-spit_dt-d)
-    
-    return res
-'''
 
 def display_shell(alpha):
     mv = get_alp2040(side_compartment_height - 20*2).rotateZ(deg(90))
@@ -122,24 +92,35 @@ def display_shell_mounts():
     m = m.left(gap_dropout_width/2+10) + m.right(gap_dropout_width/2+10)
     m = m.down(20)
 
-    '''
-    con = get_con2020()
-    con = con.up(h/2-10)
-    con = con.back(10)
-    dt = gap_dropout_width/2+17/2 + (20-17)/2
-    m += con.left(dt)
-    m += con.right(dt)
-    '''
-    con = get_con2020().rotateZ(deg(-90))
-    con = con.up(h/2-10)
-    m += con.left(gap_dropout_width/2+20)
-    m += con.rotateZ(deg(180)).right(gap_dropout_width/2+20)
+    conl = con2040s
+    conr = conl.rotateY(deg(-90))
+    dtl = gap_dropout_width/2+20+38.5/2
+    conl = conl.left(dtl)
+    conr = conr.right(dtl)
+    con = conl + conr
+    con = con.up(h/2-38.5/2)
+    m += con
     m += get_alp2020(dropout_width + 20*2).rotateY(deg(90)).up(h / 2 - 10)
 
     dt_holes_back = 0
     sole_thick = gap_dropout_height - gap_dropout_sole_pos    
     cov = box(dropout_width + 20*2, cover_thickness, h + 20, center=True)
     
+    '''
+    d = hole_tap_d[5]
+    n = cylinder(d/2, nut_m5_len, True).rotateX(deg(90))
+    holes = n.up((h+20)/2 - 10).left((dropout_width+20*2)/2 - 10)
+    holes += holes.mirrorYZ()
+    holes = holes.down(20) + holes.mirrorXY()
+
+    holes0 = n.up((h+20)/2 - 10).left((dropout_width+20*2)/2 - 30)
+    holes0 += holes0.mirrorYZ()
+    holes += holes0
+    cov -= holes
+    cut = box(20, cover_thickness, 20, center=True).up((h+20)/2-10).left((dropout_width + 20*2)/2 - 10)
+    cut += cut.mirrorYZ()
+    cov -= cut
+    '''
     d = hole_tap_d[5]
     n = cylinder(d/2, nut_m5_len, True).rotateX(deg(90))
     holes = n.up((h+20)/2 - 10).left((dropout_width+20*2)/2 - 10)
@@ -152,12 +133,17 @@ def display_shell_mounts():
     cov -= cylinder(gap(mcm5dropout.wheel_axle_big_d/2), 4, True).rotateX(deg(90)).back(dt_holes_back).up((gap_dropout_height-sole_thick-4)/2-dropout_m_axle_pos)
 
     cov = cov.down(10+10).back(10 + cover_thickness/2)
-    m += cov
+    #m += cov
 
     m = m.down(h/2 - dropout_m_axle_pos - 20)
     m = m.back(wheel_arch_width/2 + gap_dropout_depth - 10)
     m += m.mirrorXZ()
     display(m)
+    
+    cov = cov.down(h/2 - dropout_m_axle_pos - 20)
+    cov = cov.back(wheel_arch_width/2 + gap_dropout_depth - 10)
+    cov += cov.mirrorXZ()
+    display(cov, color=(0.5, 0.75, 0.5, 0.25))
 
 def display_wheel():
     shell_height_half = (side_compartment_height) / 2
